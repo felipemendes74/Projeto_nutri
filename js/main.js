c73040784d2ef1cc5a6c0dc86e939010743d79c9
felipemendes74 <felipe.mendes74@etecia.com.br>
2020-02-20 19:38:57 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+let pacientes = document.querySelectorAll(".paciente");
+
+pacientes.forEach(calcularImc);
+
+function    calcularImc(paciente){
+//for (var i = 0; i < pacientes.length; i++) {
+//    let paciente = pacientes[i];
+
+    //console.error("vai trabalhar");
+    const titulo = document.querySelector("#titulo");
+    titulo.textContent = "Etecia nutrição e dietética";
+
+
+    //pegar peso
+    let peso = paciente.querySelector(".info-peso").textContent;
+    //pegar altura
+    let altura = paciente.querySelector(".info-altura").textContent;
+
+    //validar dados
+    let pesoValido = true;
+    let alturaValido = true;
+    let tdIMC = paciente.querySelector(".info-imc");
+    
+    if (peso <= 0 || peso > 1000) {
+        pesoValido = false;
+        tdIMC.textContent = "Peso inválido";
+        paciente.classList.add("paciente-invalido");
+    }
+    if (altura <= 0 || altura > 1000) {
+        alturaValido = false;
+        tdIMC.textContent = "Altura inválida";
+        paciente.classList.add("paciente-invalido");
+    }
+    //calcular o IMC
+    if (pesoValido && alturaValido) {
+        let imc = peso / altura ** 2;
+        // console.log(imc);
+        //escrever o IMC na tabela
+        tdIMC.textContent = imc.toFixed(2);
+    }
+}
